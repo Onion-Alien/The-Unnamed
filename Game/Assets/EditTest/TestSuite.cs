a953e8cfb57c4ea301ce22bd6b8ab800ab604193
dqp7936 <dqp7936@autuni.ac.nz>
2021-05-28 23:15:34 +1200

    update
    update

---
+++ Game/Assets/EditTest/TestSuite.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+     public class TestSuite
+    {
+        [Test]
+        public void Movement()
+        {
+        }
+    }
+
+}
